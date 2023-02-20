1ed4705586fa7251098db10198a01f12a123087a
ktk8916 <ktk8916@naver.com>
2023-02-20 10:13:39 +0900

    init

---
+++ src/test/java/com/mallohaja/letsspeak/LetsspeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mallohaja.letsspeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsspeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
